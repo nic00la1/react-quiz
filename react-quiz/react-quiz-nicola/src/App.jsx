d967dad8228c5b3ae707b95d60f7aaaa83cb6cd3
Nicola <nic00la.kaleta@gmail.com>
2025-11-01 20:33:30 +0100

    Przycisk restartuj quiz

--- react-quiz/react-quiz-nicola/src/App.jsx
+++ react-quiz/react-quiz-nicola/src/App.jsx
@@ -32,13 +32,24 @@ function App() {
     }
   };
 
+  const restartQuiz = () => () => {
+    setCurrentQuestion(0); // resetuje do pierwszego pytania
+    setScore(0); // resetuje wynik
+    setSelectedAnswer(null); // resetuje zaznaczoną odpowiedź 
+    setShowFeedback(false); // ukrywa informację zwrotną
+    setIsFinished(false); // ustawia quiz jako nieukończony
+  }
+
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center p-4 space-y-8">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-purple-600 mb-2">Nicola - Aplikacja Quizów</h1>
         <p className="text-gray-400">Sprawdź swoją wiedzę na temat Polski!</p>
       </div>
-      <QuestionCard 
+      
+      {!isFinished ? (
+        <>
+        <QuestionCard 
         showFeedback={showFeedback}
         onAnswer={handleAnswer} 
         data={questions[currentQuestion]} 
@@ -53,6 +64,20 @@ function App() {
           </button>
         )}
       </div>
+        </>
+      ) : (
+        <div className="text-center">
+          <h2 className="text-3xl font-bold mb-4">Gratulacje! Zakończyłeś quiz!</h2>
+          <p className="text-xl mb-6">Twój wynik: <span>{score}</span> z <span className="font-bold">{questions.length}</span> {" "}
+             - co daje: <span className="font-semibold">{Math.round((score / questions.length) * 100)}%</span>
+          </p>
+          <button 
+            className="bg-linear-to-r from-indigo-600 to-purple-600 py-3 px-6 rounded-lg font-medium shadow-lg cursor-pointer" 
+            onClick={restartQuiz()}>
+            Zresetuj Quiz
+          </button>
+        </div>
+      )}
     </div>
   );
 }
