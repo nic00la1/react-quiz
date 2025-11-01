503de5ff2a29f711a3dc00605a4f53e94da88a65
Nicola <nic00la.kaleta@gmail.com>
2025-11-01 19:10:38 +0100

    podstawka - main

--- react-quiz/react-quiz-nicola/src/App.jsx
+++ react-quiz/react-quiz-nicola/src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
  
   return (
-    <div>
-      <p>React quiz app</p>
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-purple-600 mb-2">Nicola - Aplikacja Quizów</h1>
+        <p className="text-gray-400">Sprawdź swoją wiedzę na temat Polski!</p>
+      </div>
     </div>
   );
 }
