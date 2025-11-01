a00484580f5f2a0e86cd953c759b9ba590b9e50d
Nicola <nic00la.kaleta@gmail.com>
2025-11-01 19:32:02 +0100

    jesli user kliknie poprawna odpowiedz - dodaje 1pkt

--- react-quiz/react-quiz-nicola/src/App.jsx
+++ react-quiz/react-quiz-nicola/src/App.jsx
@@ -4,14 +4,31 @@ import { questions } from "./data/questions";
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
- 
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [showFeedback, setShowFeedback] = useState(false);
+
+  const handleAnswer = (option) => {
+    if (showFeedback) return; // zapobiega wielokrotnemu kliknięciu na odpowiedź 
+
+    setSelectedAnswer(option); 
+    setShowFeedback(true); 
+
+    // aktualizacja wyniku, jeśli odpowiedź jest poprawna
+    if (option === questions[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center p-4 space-y-8">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-purple-600 mb-2">Nicola - Aplikacja Quizów</h1>
         <p className="text-gray-400">Sprawdź swoją wiedzę na temat Polski!</p>
       </div>
-      <QuestionCard data={questions[currentQuestion]} />
+      <p>Wynik: {score}</p>
+      <QuestionCard onAnswer={handleAnswer} data={questions[currentQuestion]} />
     </div>
   );
 }
