c6f2f3b9fb08c1cec908069ea28ead97fd9f5135
Nicola <nic00la.kaleta@gmail.com>
2025-11-01 19:06:12 +0100

    tworzenie tablicy pytań

---
+++ react-quiz/react-quiz-nicola/src/data/questions.js
@@ -0,0 +1,152 @@
+export const questions = [
+	{
+		question: "Jaka jest stolica Polski?",
+		options: ["Kraków", "Warszawa", "Gdańsk", "Wrocław"],
+		answer: "Warszawa",
+	},
+	{
+		question: "Który polski pisarz napisał 'Lalkę'?",
+		options: ["Bolesław Prus", "Henryk Sienkiewicz", "Stanisław Lem", "Adam Mickiewicz"],
+		answer: "Bolesław Prus",
+	},
+	{
+		question: "Ile wynosi 7 × 6?",
+		options: ["42", "36", "56", "48"],
+		answer: "42",
+	},
+	{
+		question: "Które zwierzę jest symbolem Polski?",
+		options: ["Orzeł biały", "Żubr", "Wilk", "Sokół"],
+		answer: "Orzeł biały",
+	},
+	{
+		question: "Jak nazywa się największe polskie jezioro?",
+		options: ["Śniardwy", "Mamry", "Niegocin", "Jeziorak"],
+		answer: "Śniardwy",
+	},
+	{
+		question: "W którym roku Polska odzyskała niepodległość po I wojnie światowej?",
+		options: ["1918", "1939", "1914", "1920"],
+		answer: "1918",
+	},
+	{
+		question: "Który z tych języków jest językiem programowania?",
+		options: ["HTML", "CSS", "JavaScript", "Markdown"],
+		answer: "JavaScript",
+	},
+	{
+		question: "Jaki jest narodowy napój wielu Polaków podawany zwykle rano?",
+		options: ["Herbata", "Kawa", "Kompot", "Sok"],
+		answer: "Kawa",
+	},
+	{
+		question: "Jak nazywa się długi, tradycyjny polski obiad złożony z zupy i drugiego dania?",
+		options: ["Śniadanie", "Podwieczorek", "Obiad", "Kolacja"],
+		answer: "Obiad",
+	},
+	{
+		question: "Które miasto jest znane z Wawelu i Sukiennic?",
+		options: ["Poznań", "Kraków", "Łódź", "Szczecin"],
+		answer: "Kraków",
+	},
+	{
+		question: "Jak nazywa się najwyższy szczyt Polski?",
+		options: ["Rysy", "Giewont", "Śnieżka", "Śnieżnik"],
+		answer: "Rysy",
+	},
+	{
+		question: "Z jakiego kraju pochodzi sushi?",
+		options: ["Chiny", "Korea", "Japonia", "Tajlandia"],
+		answer: "Japonia",
+	},
+	{
+		question: "Ile jest kontynentów na Ziemi?",
+		options: ["5", "6", "7", "8"],
+		answer: "7",
+	},
+	{
+		question: "Co mierzy termometr?",
+		options: ["Ciśnienie", "Temperaturę", "Prędkość", "Wilgotność"],
+		answer: "Temperaturę",
+	},
+	{
+		question: "Który gaz jest niezbędny do oddychania dla ludzi?",
+		options: ["Azot", "Tlen", "Dwutlenek węgla", "Hel"],
+		answer: "Tlen",
+	},
+	{
+		question: "Jaki jest symbol chemiczny wody?",
+		options: ["O2", "H2O", "CO2", "NaCl"],
+		answer: "H2O",
+	},
+	{
+		question: "Które święto obchodzimy 1 maja w Polsce?",
+		options: ["Święto Niepodległości", "Święto Pracy", "Boże Narodzenie", "Wielkanoc"],
+		answer: "Święto Pracy",
+	},
+	{
+		question: "Jak nazywa się polski astronom, który sformułował heliocentryczny model Układu Słonecznego?",
+		options: ["Mikołaj Kopernik", "Jan Heweliusz", "Nikola Tesla", "Marie Curie"],
+		answer: "Mikołaj Kopernik",
+	},
+	{
+		question: "Ile minut trwa standardowy mecz piłki nożnej (bez dogrywki)?",
+		options: ["60", "90", "120", "45"],
+		answer: "90",
+	},
+	{
+		question: "Jak nazywa się festiwal filmowy odbywający się w Cannes?",
+		options: ["Międzynarodowy Festiwal Filmowy w Cannes", "Sundance", "Berlinale", "Wenecja"],
+		answer: "Międzynarodowy Festiwal Filmowy w Cannes",
+	},
+	{
+		question: "Jakie zwierzę daje mleko, z którego produkuje się ser mozzarella?",
+		options: ["Krowa", "Koza", "Bawół", "Owca"],
+		answer: "Bawół",
+	},
+	{
+		question: "Który instrument ma klawisze i jest często używany w muzyce klasycznej?",
+		options: ["Gitara", "Skrzypce", "Fortepian", "Trąbka"],
+		answer: "Fortepian",
+	},
+	{
+		question: "Jak nazywa się największy ocean na Ziemi?",
+		options: ["Ocean Atlantycki", "Ocean Spokojny", "Ocean Indyjski", "Ocean Arktyczny"],
+		answer: "Ocean Spokojny",
+	},
+	{
+		question: "Co oznacza skrót 'WWW' w Internecie?",
+		options: ["World Wide Web", "Wide Web World", "Web World Wide", "World Web Wide"],
+		answer: "World Wide Web",
+	},
+	{
+		question: "Ile jest podstawowych kolorów w modelu RGB?",
+		options: ["2", "3", "4", "6"],
+		answer: "3",
+	},
+	{
+		question: "W jakich dziedzinach Maria Skłodowska-Curie otrzymała Nagrody Nobla?",
+		options: ["Fizyka i chemia", "Chemia i medycyna", "Literatura i Pokój", "Medycyna i fizyka"],
+		answer: "Fizyka i chemia",
+	},
+	{
+		question: "Z czego robi się bigos?",
+		options: ["Mięso i kapusta", "Ryba i ziemniaki", "Mleko i mąka", "Jajka i cukier"],
+		answer: "Mięso i kapusta",
+	},
+	{
+		question: "Który z tych ptaków nie lata?",
+		options: ["Pingwin", "Orzeł", "Sikorka", "Jaskółka"],
+		answer: "Pingwin",
+	},
+	{
+		question: "Jaka jest waluta Polski?",
+		options: ["Euro", "Złoty", "Dolar", "Frank"],
+		answer: "Złoty",
+	},
+	{
+		question: "Jak nazywa się najdłuższa rzeka w Polsce?",
+		options: ["Wisła", "Odra", "Warta", "Bug"],
+		answer: "Wisła",
+	},
+];
